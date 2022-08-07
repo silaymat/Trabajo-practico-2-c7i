b59d7d93bce9a9082cd7ed0234eb88fb14db4c6d
silvia Aymat <silaymat@yahoo.com.ar>
2022-08-07 12:44:26 -0300

    Ejercicio-17

--- js/ejercicio-17.js
+++ js/ejercicio-17.js
@@ -1,2 +1,12 @@
 //17- Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
 
+let texto = prompt("Ingrese un texto");
+
+for(let i = 0; i < texto.length; i++){
+    let primeraLetra = texto[i];
+    if(texto[i] == "a" || texto[i] == "e" || texto[i] == "i" || texto[i] == "o" || texto[i] == "u"){
+        document.write(primeraLetra + ": esta es la primera vocal, y esta en la posicion : " + i + " del array");
+        break;
+    }
+}
+
